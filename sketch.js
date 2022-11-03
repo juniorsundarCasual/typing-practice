3cf173f18b1e0d968fb1cddf1c7f7ef1c527680d
juniorsundar <juniorsundar@gmail.com>
2022-11-03 16:37:31 +0400

    Updated for detecting paragraph

--- sketch.js
+++ sketch.js
@@ -1,6 +1,14 @@
+let returnTags = [0];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255);
+
+  let textArea = new p5.Element(document.getElementById("TextArea"));
+  console.log(document.getElementById("TextArea"));
+  textArea.position(10,10);
+  textArea.size(windowWidth/2-20,windowHeight-20);
+  textArea.input(myInputEvent);
   // put setup code here
 }
 
@@ -14,4 +22,18 @@ function draw() {
     fill(255);
     ellipse(mouseX, mouseY, 80, 80);
   }
+}
+
+function myInputEvent() {
+  let last = this.value()[this.value().length-1];
+  if(last == "\n"){
+    returnTags.push(this.value().length);
+    for (let i = 1; i < returnTags.length; i++){
+      let text = this.value().slice(returnTags[i-1], returnTags[i]);
+      textSize(20);
+      textWrap(WORD);
+      text(text, windowWidth/2+10, 20, windowWidth/2-20,windowHeight-20);
+    }
+  }
+
 }
