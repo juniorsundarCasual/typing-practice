daaa1c89006da566e12db43f14c8f8cf796345c7
juniorsundar <juniorsundar@gmail.com>
2022-11-03 12:38:58 +0400

    Updates

--- sketch.js
+++ sketch.js
@@ -8,9 +8,11 @@ function draw() {
   // background(255);
   if (mouseIsPressed){
     fill(0);
+    ellipse(mouseX, mouseY, 80, 80);
   } else {
+    background(255);
     fill(255);
+    ellipse(mouseX, mouseY, 80, 80);
   }
-  ellipse(mouseX, mouseY, 80, 80);
   // put drawing code here
 }
