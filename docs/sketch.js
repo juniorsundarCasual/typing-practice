0c3e1601fd93266b4d3b72ad0cbf0770c6d30375
juniorsundar <juniorsundar@gmail.com>
2022-11-03 09:23:44 +0400

    fixed gitignore

---
+++ docs/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+  // put setup code here
+}
+
+function draw() {
+  // background(255);
+  if (mouseIsPressed){
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
+  // put drawing code here
+  s
+}
